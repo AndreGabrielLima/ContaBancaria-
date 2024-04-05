fd822fed5a22de34a785152984485ec9aaae3c59
André Gabriel <andregabriel_lima@hotmail.com.br>
2024-04-05 18:49:27 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite a agência: ");
+        String agencia = sc.next();
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nome = sc.next();
+
+        System.out.println("Por favor, digite seu saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá, " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        
+    }
+}
